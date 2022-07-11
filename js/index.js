a9565bc28f8818ba71135396b035423bed4a4083
mchArg <m.christello@hotmail.com>
2022-07-11 14:02:43 -0300

    Disney Practica V-6 Contacto

---
+++ js/index.js
@@ -0,0 +1,73 @@
+console.log("Segunda forma de escribir JS");
+
+// Formas de comentar una variable
+// Primera forma
+var perro = "Arya"
+console.log(perro)
+
+// Segunda forma
+let perro2 = "Rocky"
+perro2 = "Benito"
+console.log(perro2)
+
+let nota
+// declaramos un valor
+nota = 10
+let mitad = nota/2
+console.log(nota)
+console.log(mitad)
+console.log(mitad*4);
+
+// Tercera forma
+// Variable CONST
+const profesion = 'seguros'
+console.log(profesion)
+
+// Tipos de datos
+// String
+let nombre = 'matias'
+
+// Number
+let dni = 32879540
+console.log(dni);
+
+// Booleans
+let definicion = true
+console.log(definicion)
+
+// Prompt - forma de ingresar datos
+/* let nombreMascota = prompt('ingrese el nombre de su mascota')
+let nombreDueno = prompt('ingrese el nombre del dueño')
+
+// Dos formas de concatenar
+console.log('el nombre del dueño es '+nombreDueno+ '. Gracias por trar a '+nombreMascota+ ' a nuestra veterinaria');
+alert('el nombre de su mascota es ${nombreMascota}, gracias por traerla a nuestra vetarinaria, ${nombreDueno}')
+
+
+let nota1 = prompt("ingrese la primera nota")
+let nota2 = prompt('ingrese segunda nota')
+
+let nota1 = 4
+let nota2 = 10
+console.log(nota1+nota2);
+
+
+ */
+
+
+
+// NaN - Not a Number - Quiere decir que estamos realizando operaciones entre dos datos que no son numéricos
+
+
+
+// Calcule el priomedio de gol de un jugador/a a partir del ingreso de la cantidad de partidos y goles.
+/* let nombreJugador = prompt('Ingrese el nombre del Jugador/a')
+let cantGoles = prompt('Ingrese la cantidad del goles del jugador/a en la temporada')
+let cantPartidos = prompt('Ingrese la cantidad de partidos jugados')
+let promedio = cantGoles/cantPartidos
+alert(`El promedio de goles de ${nombreJugador} es de ${promedio.toFixed(2)}`)
+
+console.log(promedio);
+ */
+
+
