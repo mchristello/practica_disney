578ae1794f7099c7027f0f0e7d42271c3c0800b2
mchArg <m.christello@hotmail.com>
2022-07-12 11:25:38 -0300

    Disney Practiva V-9 Menu Hambur

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ console.log(profesion)
 let nombre = 'matias'
 
 // Number
-let dni = 32879540
+let dni = 21874958
 console.log(dni);
 
 // Booleans
@@ -71,3 +71,79 @@ console.log(promedio);
  */
 
 
+// Estructura if básica
+
+let preguntaSeguro = "si"
+    // Dos == compara, un = asigna valor
+if (preguntaSeguro == "si") {
+    // Este bloque de código depende de la evaluación de la condición
+    console.log("Vamos a solicitarle unos datos para poder cotizar")
+    }else {
+        console.log("Estos son nuestros teléfonos para cuando necesite cotizar te pongas en contacto con nosotros")
+    }
+
+    let cuota = 1600
+    if (cuota < 1000) {
+        console.log("Cobertura RC");
+    }else if (cuota < 2000) {
+        console.log("Cobertura Robo Total");
+    }else if (cuota < 4000) {
+        console.log("Cobertura Total y Parcial");
+    }else {
+        console.log("Cobertura Todo Riesgo");
+    }
+
+
+    // Variable booleana
+    let numero = 10
+    let mayorA5 = (numero > 5)
+    console.log(mayorA5)
+    if (mayorA5){
+        console.log("EL numero es mayor a 5")
+    }else {
+        console.log("El numero no es mayor a 5");
+    }
+
+
+    // if equivalente peros in usar else if
+    let color = ""
+
+    if (color == "") {
+        console.log("Aqui no se ingresó ningún dato");
+    }else{
+        if ((color.toLowerCase() == "rojo") || (color.toUpperCase() == "AMARILLO") || (color.toLowerCase() == "azul")) {
+            console.log("Este es un color primario")
+        }else {
+            console.log("Este no es un color primario")
+        }
+    }
+
+    if (color != "") {        
+        if ((color.toLowerCase() == "rojo") || (color.toUpperCase() == "AMARILLO") || (color.toLowerCase() == "azul")) {
+            console.log("Este es un color primario")
+        }else {
+            console.log("Este no es un color primario")
+        }
+    }else {
+        console.log("Aqui no se ingresó ningún dato")
+    }
+
+
+    // EJERCICIO
+    // Pedir una nota al usuario y con el uso de condicionales evaluar:
+    // Si la nota es mayor a 10 o menor a 0, devolver que no es una nota válida
+    // En caso contrario, si la nota es igual a 10 calificar con excelente. Si es mayor a 7 calificar con promocionado, de ser mayor o igual a 4 considerar aprobado y menor a 4 como desaprobado.
+    
+    let evaluar = 11
+
+    if(evaluar > 10 || evaluar < 0) {
+        console.log("Nota invalida, por favor revisar")
+    }else if(evaluar == 10){
+        console.log("Excelente")
+    }else if(evaluar >= 7){
+        console.log("Promocionado")
+    }else if(evaluar >= 4){
+        console.log("Aprobado")
+    }else{
+        console.log("Desaprobado")
+    }
